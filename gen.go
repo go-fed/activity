574f22607ba186acf0cfb8314baaa594a129a5b3
Cory Slep <coryslep1@mac.com>
2020-07-05 21:58:08 +0200

    Add forgefed vocabulary to go generate command

--- gen.go
+++ gen.go
@@ -1,4 +1,4 @@
 // +build generate
-//go:generate go run ./astool -spec astool/activitystreams.jsonld -spec astool/security-v1.jsonld -spec astool/toot.jsonld -path github.com/go-fed/activity ./streams
+//go:generate go run ./astool -spec astool/activitystreams.jsonld -spec astool/security-v1.jsonld -spec astool/toot.jsonld -spec astool/forgefed.jsonld -path github.com/go-fed/activity ./streams
 
 package activity
